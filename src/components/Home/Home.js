6a278107fe630bbb04848dc11d71ccc6f1e358ea
Rutuja Dhanawade <53823042+rutujadhanawade@users.noreply.github.com>
2021-07-02 23:35:54 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -17,10 +17,10 @@ export default function Home() {
                 <div id="about">
                     <h1>About Us</h1>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                       The current pandemic has affected the education system all over the world. Everyone is learning remotely. It is still all right for the senior students but the junior students are the ones who need regular interaction and our current education system is not able to provide this to them.
                     </p>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    We created an AR-based website EduAR where students from any background can visit the site and learn various concepts with the help of 3D Models, which makes learning fun and effortless. EduAR can help students achieve better results through visualization and full immersion in the subject matter. Interactive AR learning can have a significant positive impact on students. It keeps them engaged throughout the lesson We also created a chatbot and multi-language support in our website which makes it even more convenient for students to learn.
                     </p>
 
                 </div>
